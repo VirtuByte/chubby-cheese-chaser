5145997e3e02b75d25f4f16e18f6191e9cfd0a94
Koenigseder <80044581+Koenigseder@users.noreply.github.com>
2025-11-09 18:26:14 +0100

    fix: remove debug log

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -4,9 +4,6 @@ signal score_changed(new_score: int)
 
 var score = 0
 
-func _ready() -> void:
-	print("NEW")
-
 func add_score(value: int) -> void:
 	score += value
 	emit_signal("score_changed", score)
